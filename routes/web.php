38a0574392a6ef0eaedc2a58167a6293c8776a17
Samuele-Benato <samuele.benato@gmail.com>
2023-10-12 14:50:51 +0200

    feat : add start structure

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $title = 'homepage';
+    return view('home', compact('title'));
+})->name('home');
